9e387bd208613c3700e07ea1b9294ed00bf17ecc
Kevin Oloomi <k.oloomi@wustl.edu>
2022-11-08 19:18:24 -0600

    hello

--- 438-final-project/MapViewController.swift
+++ 438-final-project/MapViewController.swift
@@ -13,16 +13,5 @@ class MapViewController: ViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//
     }
-    
-    let latitude: CLLocationDegrees = 40.0
-    let longitude: CLLocationDegrees = -76.0
-    
-    let regionDistance:CLLocationDistance = 1000
-    let coordinates = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-    let region = MKCoordinateRegion.init(coordinates,regionDistance,regionDistance)
-    
-    
-
 }
--- CalendarObject.swift
+++ CalendarObject.swift
@@ -15,6 +15,25 @@ import MapKit
 //    let time: time_value
 //}
 //
+
+
+class Buildings: NSObject, MKAnnotation {
+    let title : String?
+    let locationName: String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, locationName: String, coordinate: CLLocationCoordinate2D){
+        self.title = title
+        self.locationName = locationName
+        self.coordinate = coordinate
+    }
+    
+    var subtitle: String?{
+        return locationName
+    }
+    
+}
+
 struct Building{
     let name: String
     let coords: CLLocation
