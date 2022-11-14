dcd50a6cf75a7c4d63375906ccf1c2800b6cfc37
orosand123 <orosand@wustl.edu>
2022-11-14 12:31:20 -0600

    Building Search

--- 438-final-project/SearchViewController.swift
+++ 438-final-project/SearchViewController.swift
@@ -6,12 +6,75 @@
 //
 
 import Foundation
-class SearchViewController: ViewController {
+import CoreLocation
+import UIKit
 
+class SearchViewController: ViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating {
+    let searchController = UISearchController()
+
+    var buildings: [Buildings] = []
+    var fullBuildingList: [Buildings] = []
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return buildings.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //BuildingNames
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel!.text = buildings[indexPath.row].locationName
+        
+        //Style
+        if indexPath.row % 2 == 0 {
+            cell.backgroundColor = UIColor.systemGray5
+        }
+        else{
+            cell.backgroundColor = UIColor.systemGray6
+        }
+        
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        }
+        
+    func loadData() -> [Buildings]{
+        let path = Bundle.main.path(forResource: "building list", ofType: "plist")!
+        let url = URL(fileURLWithPath: path)
+        let data = try! Data(contentsOf: url)
+        let decoder = PropertyListDecoder()
+        let arr = try! decoder.decode([Buildings].self, from: data)
+        return(arr)
+    }
+    //ViewController
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        print("inside classes")
+        fullBuildingList = loadData()
+        buildings = fullBuildingList
+        navigationItem.searchController = searchController
+        searchController.searchResultsUpdater = self
     }
-
-
+    func updateSearchResults(for searchController: UISearchController) {
+        if let text = searchController.searchBar.text{
+            if (text != ""){
+                self.buildings = self.fullBuildingList.filter {
+                    $0.title?.lowercased().contains(text.lowercased()) ?? false
+                                }
+                
+            } else {
+                self.buildings = self.fullBuildingList
+            }
+        }
+        
+        print(buildings.count)
+        tableView.reloadData()
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
 }
