5738e54b427f6daf3a39b7a7dc4ad123a7f4bec5
kchen5477 <kchen1@wustl.edu>
2022-12-05 01:51:06 -0600

    Help Section Done

--- 438-final-project/HelpViewController.swift
+++ 438-final-project/HelpViewController.swift
@@ -13,44 +13,53 @@ class HelpViewController: UIViewController, UICollectionViewDelegate, UICollecti
 
     
     @IBOutlet weak var sliderCollectionView: UICollectionView!
-    @IBOutlet weak var pageControllerView: UIPageControl!
+//    @IBOutlet weak var pageControllerView: UIPageControl!
     
-//    var imageHelpArray = [UIImage(named: "help1"), UIImage(named: "help2")]
-    var imageHelpArray = ["help3", "help4"]
+
+    var imageHelpArray = ["help1", "help2", "help3"]
+    var imagePosition = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        pageControllerView.currentPage = imagePosition
+//        pageControllerView.numberOfPages = imageHelpArray.count
 
-        // Do any additional setup after loading the view.
     }
+    
+//    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+//        pageControllerView.currentPage = indexPath.row
+//        print(pageControllerView.currentPage)
+//    }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
         return imageHelpArray.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CollectionViewCell
-        cell?.image.image = UIImage(named: imageHelpArray[indexPath.row])
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        cell.image.image = UIImage(named: imageHelpArray[indexPath.row])
 
         
-        return cell! // forced unwrapped
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 358.0 , height: 573.0 )
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-
-        return 10.0
+        return cell
     }
 
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 10.0
-    }
+
+    //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+//    }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: 358.0 , height: 573.0 )
+//    }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//
+//        return 0.0
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//        return 0.0
+//    }
 
 
 
